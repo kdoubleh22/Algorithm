7e1bfe8d472ef3c0f5355d2187e62c1bdd680c32
hh <kdoubleh22@naver.com>
2023-01-02 23:16:32 +0900

    ing

---
+++ Algorithm/src/bj/s3/BJ_S3_09095.java
@@ -0,0 +1,25 @@
+package bj.s3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_S3_09095 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	static int _n;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		_n=Integer.parseInt(input.readLine());
+		
+		for(int i=0;i<_n;i++) {
+			
+		}
+		
+		System.out.println(output);
+	}
+
+}
