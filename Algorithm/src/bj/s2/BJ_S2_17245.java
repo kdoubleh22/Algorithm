a9da96ed1d5dee5ca632877b346fcf3208158804
hh <kdoubleh22@naver.com>
2023-01-15 16:46:51 +0900

    ing

--- Algorithm/src/bj/s2/BJ_S2_17245.java
+++ Algorithm/src/bj/s2/BJ_S2_17245.java
@@ -3,35 +3,89 @@ package bj.s2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+ * -풀이
+ * 
+ */
+
 public class BJ_S2_17245 {
-    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder output = new StringBuilder();
-    static StringTokenizer tokens;
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
 
-    static int _n;
-    static int[] _heights;
-    static int _on;
-    static long _total;
-    public static void main(String[] args) throws IOException {
-        _n=Integer.parseInt(input.readLine());
+	static int _n;
+	static int[] _heights;
+	static long _total;
 
-        _heights=new int[10000001];
-        _total=0;
-        for(int i=0;i<_n;i++){
-            tokens = new StringTokenizer(input.readLine());
-            for(int j=0;j<_n;j++){
-                _heights[Integer.parseInt(tokens.nextToken())]+=1;
+	public static void main(String[] args) throws IOException {
+		_n = Integer.parseInt(input.readLine());
 
-            }
-        }
+		_heights = new int[_n * _n]; // 각 칸에 몇 대씩 쌓여있는지.
+		_total = 0; // 컴퓨터 전체 개수.
+		int idx = 0;
+		for (int i = 0; i < _n; i++) {
+			tokens = new StringTokenizer(input.readLine());
+			for (int j = 0; j < _n; j++) {
+				int computers = Integer.parseInt(tokens.nextToken());
+				_heights[idx++] = computers;
+				_total += computers;
+			}
+		}
 
-        _on=0;
-        for(int t=0;t<10000001;t++){
-            _on+=_heights[t];
-            if(_on>=)
-        }
+		int totalCnt = _n * _n; // 칸의 개수.
 
-    }
+		Arrays.sort(_heights);
+
+//		System.out.println(Arrays.toString(_heights));
+
+		long on = 0; // 켜진 컴퓨터의 개수.
+		long half = (_total + 1) / 2; // 전체 컴퓨터 절반의 개수.
+		int higherIdx = 0; // 현재 고려한 층보다 높은 층에 있는 컴퓨터의 칸 수.
+
+		// 0대씩 쌓여있는건 고려하지 않음.
+		while (_heights[higherIdx] == 0) {
+			higherIdx += 1;
+			if (higherIdx == totalCnt) { // 전부 0이라는 소리.
+				break;
+			}
+		}
+
+//		System.out.println("half:" + half);
+		int answer = -1;
+
+		if (higherIdx == totalCnt) { // 전부 0일 때는 답이 0.
+			answer = 0;
+		} else {
+			for (int t = 1; t < 10000001; t++) {
+//				System.out.println("higherIdx:" + higherIdx);
+
+				on += totalCnt - higherIdx;
+
+				int curHeight = _heights[higherIdx];
+
+				while (_heights[higherIdx] <= curHeight) {
+					higherIdx += 1;
+					if (higherIdx == totalCnt) {
+						higherIdx -= 1;
+						break;
+					}
+
+				}
+
+				System.out.println("time:" + t + " on:" + on);
+
+				// 종료조건
+				if (on >= half) {
+					answer = t;
+					break;
+				}
+
+			}
+		}
+
+		System.out.println(answer);
+	}
 }
