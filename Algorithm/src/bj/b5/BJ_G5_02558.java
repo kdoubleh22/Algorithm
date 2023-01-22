8caa5c68fa4056fe90a945f447dcbaead2cce380
hh <kdoubleh22@naver.com>
2023-01-22 23:28:36 +0900

    구현

---
+++ Algorithm/src/bj/b5/BJ_G5_02558.java
@@ -0,0 +1,16 @@
+package bj.b5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_G5_02558 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		int A = Integer.parseInt(input.readLine());
+		int B = Integer.parseInt(input.readLine());
+
+		System.out.println(A + B);
+	}
+
+}
