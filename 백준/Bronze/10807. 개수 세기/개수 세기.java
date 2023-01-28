b9f71375c3657d6b82d9003e7fce7f9f9d920fdb
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2023-01-28 23:51:25 +0900

    [Bronze V] Title: 개수 세기, Time: 80 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+
+	static int _n;
+	static int _v;
+	static int[] arr;
+
+	public static void main(String[] args) throws Exception {
+		_n = Integer.parseInt(input.readLine());
+
+		arr = new int[_n];
+		tokens = new StringTokenizer(input.readLine());
+		for (int i = 0; i < _n; i++) {
+			arr[i] = Integer.parseInt(tokens.nextToken());
+		}
+
+		_v = Integer.parseInt(input.readLine());
+
+		int cnt = 0;
+		for (int i = 0; i < _n; i++) {
+			if (arr[i] == _v) {
+				cnt += 1;
+			}
+		}
+
+		System.out.println(cnt);
+	}
+
+}
