ff01faaccfe11c354c618293c618295f3d7573bf
hh <kdoubleh22@naver.com>
2023-02-03 21:30:51 +0900

    구현

---
+++ Algorithm/src/bj/b5/BJ_B5_05597.java
@@ -0,0 +1,23 @@
+package bj.b5;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_B5_05597 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		boolean[] check = new boolean[31];
+		for (int i = 0; i < 28; i++) {
+			int n = Integer.parseInt(input.readLine());
+			check[n] = true;
+		}
+
+		for (int i = 1; i < 31; i++) {
+			if (!check[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
