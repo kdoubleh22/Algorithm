75dcfe00c2bc39c83883bca75866efff78157368
kdoubleh22 <kdoubleh22@gmail.com>
2023-09-25 19:56:56 +0900

    rename

--- Algorithm/src/bj/g4/BJ_G4_01753.java
+++ Algorithm/src/bj/g4/BJ_G4_01753.java
@@ -3,9 +3,7 @@ package bj.g4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -15,81 +13,77 @@ public class BJ_G4_01753 {
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 
-	static int _v, _e, _s; // _s:start
+	static int _n, _answer;
+	static int[][] _costs;
 
-	static List<Vertex>[] _graph;
+	static int[][] deltas = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
-	static void dijkstra() {
-		int[] md = new int[_v + 1];
-		Arrays.fill(md, Integer.MAX_VALUE);
-		md[_s] = 0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		_n = Integer.parseInt(input.readLine());
 
-		PriorityQueue<Vertex> pq = new PriorityQueue<>();
-		pq.offer(new Vertex(_s, 0));
-
-		while (!pq.isEmpty()) {
-			Vertex polled = pq.poll();
-
-			if (polled.weight > md[polled.to]) {
-				continue;
-			}
-
-			for (Vertex v : _graph[polled.to]) {
-				if (polled.weight + v.weight < md[v.to]) {
-					md[v.to] = polled.weight + v.weight;
-					pq.offer(new Vertex(v.to, md[v.to]));
-				}
-			}
-		}
-
-		for (int i = 1; i <= _v; i++) {
-			if (md[i] != Integer.MAX_VALUE) {
-				output.append(md[i]);
-			} else {
-				output.append("INF");
-			}
-			output.append("\n");
-		}
-
-	}
-
-	public static void main(String[] args) throws IOException {
-		tokens = new StringTokenizer(input.readLine());
-		_v = Integer.parseInt(tokens.nextToken());
-		_e = Integer.parseInt(tokens.nextToken());
-		_s = Integer.parseInt(input.readLine());
-
-		_graph = new List[_v + 1];
-		for (int i = 0; i <= _v; i++) {
-			_graph[i] = new ArrayList<>();
-		}
-
-		for (int i = 0; i < _e; i++) {
+		_costs = new int[_n][_n];
+		for (int i = 0; i < _n; i++) {
 			tokens = new StringTokenizer(input.readLine());
-			int u = Integer.parseInt(tokens.nextToken());
-			int v = Integer.parseInt(tokens.nextToken());
-			int w = Integer.parseInt(tokens.nextToken());
+			for (int j = 0; j < _n; j++) {
+				_costs[i][j] = Integer.parseInt(tokens.nextToken());
+			}
+		} // 입력
 
-			_graph[u].add(new Vertex(v, w));
-		}
-
+		_answer = -1;
 		dijkstra();
 
+		output.append(_answer);
 		System.out.println(output);
+	}
+
+	private static void dijkstra() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		int[] aws = new int[_n];
+
+		Arrays.fill(aws, Integer.MAX_VALUE); // 거리 무한대로 초기화.
+
+		// 0번 출발
+		aws[0] = 0;
+		pq.offer(new Node(0, 0));
+
+		int cnt = 0;
+		while (!pq.isEmpty()) {
+			Node head = pq.poll();
+			System.out.println(Arrays.toString(aws));
+			System.out.println("head.n:"+head.n+",head.aw:"+head.aw);
+			cnt++;
+
+			if (cnt == _n) {
+				int min = Integer.MAX_VALUE;
+				for (int i = 1; i < _n; i++) {
+					min = Math.min(min, _costs[i][0]);
+				}
+				_answer = head.aw + min;
+				return;
+			}
+
+			for (int i = 0; i < _n; i++) {
+				if (aws[i] > aws[head.n] + _costs[head.n][i]) {
+					aws[i] = aws[head.n] + _costs[head.n][i];
+					pq.offer(new Node(i, aws[i]));
+				}
+			}
+
+		}
 
 	}
 
-	static class Vertex implements Comparable<Vertex> {
-		int to, weight;
+	static class Node implements Comparable<Node> {
+		int n, aw; // accumulated weight;
 
-		public Vertex(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
+		public Node(int n, int aw) {
+			this.n = n;
+			this.aw = aw;
 		}
 
 		@Override
-		public int compareTo(Vertex o) {
-			return Integer.compare(this.weight, o.weight);
+		public int compareTo(Node o) {
+			return Integer.compare(this.aw, o.aw);
 		}
 
 	}
