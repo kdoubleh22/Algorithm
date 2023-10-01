f5f14acf34ec811cbb3628ca520535c0dc1c45b2
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2023-10-01 18:08:10 +0900

    [level 1] Title: 체육복, Time: 0.97 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+/*
+lost를 돌면서, reserve에서 있는지 확인하고, 없으면 앞에서부터 빌려줌.
+*/
+
+class Solution {
+    
+    
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        
+        Arrays.sort(lost);
+        
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for(int r : reserve){
+            hs.add(r);
+        }
+        
+        TreeSet<Integer> ts = new TreeSet<>();
+        
+        for(int l : lost){
+            ts.add(l);
+        }
+        
+        // 여벌 체육복을 가져온 학생이 체육복을 도난당함.
+        for(int l : lost){
+            if(hs.contains(l)){
+                hs.remove(l);
+                ts.remove(l);
+            }
+        }
+        
+        for(int l : ts){
+                // 먼저 앞 번호 확인.
+                if(hs.contains(l-1)){
+                    hs.remove(l-1);
+                }
+                // 뒷 번호 확인.
+                else if(hs.contains(l+1)){
+                    hs.remove(l+1);
+                }
+                // 앞 뒷 번호 둘 다 여벌 체육복이 없는 경우.
+                else{
+                    answer--;
+                }
+        }
+        
+        return answer;
+    }
+}
