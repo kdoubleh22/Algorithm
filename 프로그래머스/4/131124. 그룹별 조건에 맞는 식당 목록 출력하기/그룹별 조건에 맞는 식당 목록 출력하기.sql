ff8912f12442e706bcfbf5dd657fc838c483e820
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2024-03-12 21:47:34 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,14 +1,26 @@
+# select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE,'%Y-%m-%d') REVIEW_DATE
+# from MEMBER_PROFILE a
+# join REST_REVIEW b on a.MEMBER_ID = b.MEMBER_ID
+# where a.MEMBER_ID in (select member_id
+#                       from REST_REVIEW
+#                       group by member_id
+#                       having count(*) = (select count(*) c
+#                                          from REST_REVIEW
+#                                          group by member_id
+#                                          order by c desc
+#                                          limit 1
+#                                         )
+#                      )
+# order by REVIEW_DATE, REVIEW_TEXT;
+
 select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE,'%Y-%m-%d') REVIEW_DATE
 from MEMBER_PROFILE a
 join REST_REVIEW b on a.MEMBER_ID = b.MEMBER_ID
-where a.MEMBER_ID in (select member_id
-                      from REST_REVIEW
-                      group by member_id
-                      having count(*) = (select count(*) c
-                                         from REST_REVIEW
-                                         group by member_id
-                                         order by c desc
-                                         limit 1
-                                        )
+where a.MEMBER_ID in (select * from (select member_id
+                                     from REST_REVIEW
+                                     group by member_id
+                                     order by count(*) desc
+                                     limit 1
+                                    ) temp
                      )
 order by REVIEW_DATE, REVIEW_TEXT;
