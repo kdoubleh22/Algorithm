b52d9e925267eb7cf6f7e7abbb4661f31cc95a7d
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2024-03-12 20:56:32 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,8 +1,8 @@
-select a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
-from APPOINTMENT as a
-join PATIENT as p on a.PT_NO = p.PT_NO
-join DOCTOR as d on a.MDDR_ID = d.DR_ID
-where substring(APNT_YMD,1,10) = '2022-04-13'
-and APNT_CNCL_YMD is null
+select a.APNT_NO, b.PT_NAME, a.PT_NO, a.MCDP_CD, c.DR_NAME, a.APNT_YMD
+from appointment a
+join PATIENT b on a.PT_NO = b.PT_NO
+join doctor c on a.mddr_id = c.dr_id
+where substr(a.APNT_YMD, 1, 10) = '2022-04-13'
+and APNT_CNCL_YN = 'N'
 and a.MCDP_CD = 'CS'
-order by a.APNT_YMD
+order by apnt_ymd;
