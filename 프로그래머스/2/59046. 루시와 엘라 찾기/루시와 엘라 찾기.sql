4319596b7b5b09f7dd8c9fd0276fc27b024b48f3
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2024-03-12 20:59:21 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,9 +1,3 @@
-# select 아이디, 이름, 성별 및 중성화 여부
-# from
-# where 이름이 저 중 한 개
-# order by 아이디 순
-
-select animal_id, name, sex_upon_intake
-from animal_ins
-where name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
-order by animal_id
+select ANIMAL_ID, NAME, SEX_UPON_INTAKE
+from ANIMAL_INS
+where name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty');
