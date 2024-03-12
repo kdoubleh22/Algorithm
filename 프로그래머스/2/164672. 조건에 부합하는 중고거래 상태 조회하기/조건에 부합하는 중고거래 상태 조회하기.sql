c2482b83d704f84a7dcf7340147b7cc72aa8dd4d
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2024-03-12 19:57:40 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,15 +1,7 @@
-# select 게시글id, 작성자id, 게시글제목, 가격, (if ~ 거래상태) as status
-# from
-# where 2022년 10월 4일
-# order by 게시글id 내림차순
-
-select board_id, writer_id, title, price, (case
-                                            when status = 'SALE'
-                                            then '판매중'
-                                            when status = 'RESERVED'
-                                            then '예약중'
-                                            else '거래완료'
-                                           end) as status
+select board_id, writer_id, title, price, (case when status = 'SALE' then '판매중'
+                                           when status = 'RESERVED' then '예약중'
+                                           else '거래완료'
+                                          end) status
 from used_goods_board
-where substring(created_date,1,10) = '2022-10-05'
+where substr(CREATED_DATE,1,10) = '2022-10-05'
 order by board_id desc;
