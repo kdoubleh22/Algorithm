90927acd5ebee015d8053e3d735e85b8c2a29cd8
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2024-03-12 15:51:26 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,5 +1,12 @@
+# select a.item_id, item_name, rarity
+# from item_info a join item_tree b
+# on a.item_id = b.item_id
+# where (select rarity from item_info where item_id = b.parent_item_id) = 'RARE'
+# order by item_id desc;
+
+# 참고코드
 select a.item_id, item_name, rarity
 from item_info a join item_tree b
 on a.item_id = b.item_id
-where (select rarity from item_info where item_id = b.parent_item_id) = 'RARE'
+where b.parent_item_id in (select item_id from item_info where rarity = 'RARE')
 order by item_id desc;
