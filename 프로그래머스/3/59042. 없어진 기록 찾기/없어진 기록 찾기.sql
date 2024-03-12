f46140a3dec0794f0aac2c3abc7fd8ace051dd5f
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2024-03-12 21:53:57 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,10 +1,6 @@
-# select 동물id, 이름
-# from
-# where 입양을 간 기록은 있고, 들어온 기록이 없는 -> out엔 id가 있고, in엔 id가 없음. not in
-# order by 아이디순
-
 select ANIMAL_ID, NAME
 from ANIMAL_OUTS
-where ANIMAL_ID not in (select ANIMAL_ID
-                       from ANIMAL_INS)
-order by ANIMAL_ID
+where ANIMAL_ID not in (select animal_id
+                        from ANIMAL_INS
+                       )
+order by ANIMAL_ID;
