3c886fec758c5ce500cfd1ab31eff66e713af8be
Hyeonho Kim <73050365+kdoubleh22@users.noreply.github.com>
2024-03-22 19:25:15 +0900

    [level 2] Title: 배달, Time: 1.14 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,36 +1,34 @@
 import java.util.*;
 
 class Solution {
-    
     List<Node>[] graph;
     
-    public int solution(int N, int[][] roads, int K) {
+    public int solution(int N, int[][] road, int K) {
         int answer = 0;
         
         graph = new List[N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
         
-        for (int[] road : roads) {
-            int a = road[0];
-            int b = road[1];
-            int c = road[2];
+        for (int i = 0; i < road.length; i++) {
+            int a = road[i][0];
+            int b = road[i][1];
+            int c = road[i][2];
             
             graph[a].add(new Node(b, c));
             graph[b].add(new Node(a, c));
         }
         
         int[] dist = new int[N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        for (int i = 1; i <= N; i++) {
             dist[i] = Integer.MAX_VALUE;
         }
-        
         dijkstra(dist);
         
-        for (int i = 1; i < N + 1; i++) {
+        for(int i = 1; i <= N; i++) {
             if (dist[i] <= K) {
-                answer++;
+                ++answer;
             }
         }
 
@@ -38,32 +36,28 @@ class Solution {
     }
     
     void dijkstra(int[] dist) {
-
-        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
-            
+        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>(){
             @Override
             public int compare(Node n1, Node n2) {
-                return Integer.compare(n1.dist, n2.dist);
+                return Integer.compare(n1.cost, n2.cost);
             }
         });
-        
         
         pq.add(new Node(1, 0));
         dist[1] = 0;
         
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             Node curNode = pq.poll();
             
-            if (curNode.dist != dist[curNode.node]) {
+            if (curNode.cost != dist[curNode.node]) {
                 continue;
             }
             
-            for(Node nextNode : graph[curNode.node]) {
-                int newDist = curNode.dist + nextNode.dist;
-                
+            for (Node nextNode : graph[curNode.node]) {
+                int newDist = curNode.cost + nextNode.cost;
                 if (newDist < dist[nextNode.node]) {
-                    pq.add(new Node(nextNode.node, newDist));
                     dist[nextNode.node] = newDist;
+                    pq.add(new Node(nextNode.node, newDist));
                 }
             }
         }
@@ -71,11 +65,11 @@ class Solution {
     
     class Node {
         int node;
-        int dist;
+        int cost;
         
-        public Node(int node, int dist) {
+        public Node (int node, int cost) {
             this.node = node;
-            this.dist = dist;
+            this.cost = cost;
         }
     }
 }
